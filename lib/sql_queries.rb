2d81ae55002c8d76d0775a4007156336bb430863
Blythe Pollard <blytheepollard@gmail.com>
2019-11-08 22:29:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   #How much did each user pledge total?
-  "SELECT users.name, SUM(pledges.amount) AS summed_amount FROM users JOIN pledges on users.id = pledges.user_id GROUP BY users.name ORDER BY summed_amount"
+  "SELECT users.name, SUM(pledges.amount) AS summed_amount FROM users JOIN pledges on users.id = pledges.user_id GROUP BY users.name ORDER BY summed_amount asc"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
