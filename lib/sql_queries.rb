914872f35db582ea74c0c25f1cc8cf999146b6cb
Blythe Pollard <blytheepollard@gmail.com>
2019-11-08 22:33:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,5 +30,5 @@ end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   #Who pledged and how much in books category?
-  "Write your SQL query Here"
+  "SELECT projects.category, pledges.amount FROM projects JOIN pledges ON pledges.project_id = projects.id WHERE projects.category = 'books'"
 end
